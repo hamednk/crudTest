0e433b474a807ac7b137380c1c44f074e8528c58
Mason Chase <mason.chase@smartmed.world>
2021-12-23 11:56:39 +0100

    Review: Removed unused classes, there are too many

--- Mc2.CrudTest.Application/DTO/Customer/CustomerDTO.cs
+++ Mc2.CrudTest.Application/DTO/Customer/CustomerDTO.cs
@@ -3,17 +3,6 @@ using System;
 
 namespace Mc2.CrudTest.Application.DTO.Customer
 {
-    public class CustomerDTO
-    {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public DateTime? DateOfBirth { get; set; }
-        public string PhoneNumber { get; set; }
-        public string Email { get; set; }
-        public string BankAccountNumber { get; set; }
-        public DateTime? CreatedOn { get; set; }
-    }
-
     public class CustomerSearchDTO : BaseSearchRequestDto
     {
         public int? Id { get; set; }
